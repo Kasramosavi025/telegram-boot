e0d7412b3dce8064d754142383e43ff8bbfda98b
Kasramosavi025 <kasramosavi55@gmail.com>
2025-04-25 10:32:33 +0330

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -12,4 +12,26 @@ def handle_video(update, context):
     file = context.bot.get_file(file_id)
     file.download('downloaded_video.mp4')
     update.message.reply_text("Video received! Processing... (For now, I'll just send it back)")
-    update.message.reply_video(video=open('down
+    update.message.reply_video(video=open('downloaded_video.mp4', 'rb'))
+
+def handle_photo(update, context):
+    photo = update.message.photo[-1]
+    file_id = photo.file_id
+    file = context.bot.get_file(file_id)
+    file.download('downloaded_photo.jpg')
+    update.message.reply_text("Photo received! Processing... (For now, I'll just send it back)")
+    update.message.reply_photo(photo=open('downloaded_photo.jpg', 'rb'))
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.video, handle_video))
+    dp.add_handler(MessageHandler(Filters.photo, handle_photo))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
